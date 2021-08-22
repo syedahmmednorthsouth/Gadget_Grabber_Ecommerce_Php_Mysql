c5de278223002c002f13f4abab14367ae62305a8
Syed Ahmmed <syed.ahmmed.nsu@gmail.com>
2021-08-23 00:31:47 +0600

    cart php created

---
+++ cart.php
@@ -0,0 +1,23 @@
+<?php
+//include header.php
+
+include "header.php";
+?>
+
+
+<?php
+
+include "Template/_cart-template.php";
+
+//include new-gadgets
+include "Template/_new-gadget.php";
+
+?>
+
+
+<?php
+//include footer.php
+
+include "footer.php";
+
+?>
