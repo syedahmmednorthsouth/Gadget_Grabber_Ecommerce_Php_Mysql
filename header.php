15202e4d7c27440a8f2bc6dac6e4139f7a8b35a7
Syed Ahmmed <syed.ahmmed.nsu@gmail.com>
2021-08-22 23:44:45 +0600

    Adding header.php from main design

---
+++ header.php
@@ -0,0 +1,103 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+
+  <!--Bootstarp CDN-->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous" />
+  <!--owl Carousel CDN -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+    integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css"
+    integrity="sha512-OTcub78R3msOCtY3Tc6FzeDJ8N9qvQn1Ph49ou13xgA9VsH9+LRxoFU6EqLhW4+PKRfU+/HReXmSZXHEkpYoOA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <!--font awesome cdn-->
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
+    integrity="sha512-L7MWcK7FNPcwNqnLdZq86lTHYLdQqZaz5YcAgE+5cnGmlw8JT03QB2+oxL100UeB6RlzZLUxCGSS4/++mNZdxw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <!--Custom CSS file-->
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+  <!--start #header-->
+  <header id="header">
+    <div class="strip d-flex justify-content-between px-4 py-i bg-light">
+      <p class="font-rale font-size-12 text-black-50 m-0">
+        po-2280, Rayerbazar, Atharbari, Mymensingh, Dhaka
+      </p>
+      <div class="font-rale font-size-14">
+        <a href="./signup.html" class="px-3 border-right text-dark">Sign-Up</a>
+        <a href="#" class="px-3 border-right border-left text-dark">Login</a>
+        <a href="#" class="px-3 border-right text-dark">Wishlist (0)</a>
+      </div>
+    </div>
+
+    <!--Primary Navigation-->
+
+    <nav class="navbar navbar-expand-lg navbar-dark color-second-bg">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Gadget Grabber</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav m-auto font-rubik">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">On Sale</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Catagory</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Products <i class="fas fa-chevron-down"></i></a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Catagory <i class="fas fa-chevron-down"></i></a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">Comming Soon</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+          <form action="#" class="font-size-14 font-rale">
+            <a href="#" class="py-2 rounded-pill color-primary-bg">
+              <span class="font-size-16 px-2 text-white"><i class="fas fa-shopping-cart"></i></span>
+              <span class="px-3 py-2 rounded-pill text-dark bg-light">
+                0
+              </span>
+            </a>
+          </form>
+        </div>
+      </div>
+    </nav>
+
+    <!--!Primary Navigation-->
+  </header>
+
+  <!--!start #header-->
+
+  <!--start #main-site-->
+
+  <main id="main-site">
