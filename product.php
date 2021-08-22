c0272c5f17bca0b550feb8b863049291779deeab
Syed Ahmmed <syed.ahmmed.nsu@gmail.com>
2021-08-23 00:31:31 +0600

    product php created

---
+++ product.php
@@ -0,0 +1,24 @@
+<?php
+//include header.php
+
+include "header.php";
+
+?>
+
+
+<?php
+
+include "Template/_product.php";
+
+//include top-sale
+include "Template/_top-sale.php";
+
+?>
+
+
+<?php
+//include footer.php
+
+include "footer.php";
+
+?>
